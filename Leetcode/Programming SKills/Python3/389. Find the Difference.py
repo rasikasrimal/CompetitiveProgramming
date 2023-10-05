d7234cbf22c1612172fe39f0e1019c8ae9779bd8
Rasika Srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-10-05 06:58:00 +0530

    Update 389. Find the Difference.py

--- Leetcode/Programming SKills/Python3/389. Find the Difference.py
+++ Leetcode/Programming SKills/Python3/389. Find the Difference.py
@@ -1,4 +1,12 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        diff = [char1 for char1, char2 in zip(sorted(s), sorted(t)) if char1 != char2]
-        print(diff)
+        s = sorted(s)
+        t = sorted(t)
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] != t[j]:
+                return t[j]
+            i += 1
+            j += 1
+        return t[j]
