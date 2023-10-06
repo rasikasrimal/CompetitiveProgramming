efaf96228ab3a9502658dc348bc6e4f80245c5dc
rasikasrimal <rasikacooldj620@gmail.com>
2023-10-06 06:59:54 +0530

    use sys.stdin as input

---
+++ CSES/python3/Introductory Problems/Bit Strings.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    # Write your solution code here
+    pass
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
--- CSES/python3/Introductory Problems/Increasing Array.py
+++ CSES/python3/Introductory Problems/Increasing Array.py
@@ -1,8 +1,10 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline().strip())
 count = 0
-numbers = list(map(int, input().split()))
+numbers = list(map(int, sys.stdin.readline().split()))
 for i in range(1, n):
-    if( numbers[i-1] > numbers[i]):
-        count = count + numbers[i-1] - numbers[i]
-        numbers[i-1] , numbers[i] = numbers[i] , numbers[i-1]
+    if numbers[i - 1] > numbers[i]:
+        count = count + numbers[i - 1] - numbers[i]
+        numbers[i - 1], numbers[i] = numbers[i], numbers[i - 1]
 print(count)
