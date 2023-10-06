6bd4cfeb2b28d50683841fe7849876803f0bfe70
Rasika Srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-10-06 06:32:54 +0530

    Not Optimized Solution, Time and Space complexity is high

---
+++ Leetcode/Programming SKills/Python3/242. Valid Anagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = ''.join(sorted(s))
+        sorted_t = ''.join(sorted(t))
+        return sorted_s == sorted_t
+
+        
