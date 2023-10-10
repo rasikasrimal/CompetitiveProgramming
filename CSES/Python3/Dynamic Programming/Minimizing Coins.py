a5ab5822f498257bd7968aa88445ce80e8c3f7a1
rasikasrimal <rasikacooldj620@gmail.com>
2023-10-10 08:01:51 +0530

    Time limit exceeded

--- CSES/Python3/Dynamic Programming/Minimizing Coins.py
+++ CSES/Python3/Dynamic Programming/Minimizing Coins.py
@@ -1,18 +1,17 @@
-def minimumcoins(n, coins):
-    dp = [float('inf')] * (n + 1)
+def minimumCoins(n, coins):
+    dp = [n + 1] * (n + 1)  # Initialize with a large value
     dp[0] = 0
+
     for i in range(1, n + 1):
         for coin in coins:
-            if i - coin >= 0:
+            if i - coin >= 0 and dp[i - coin] != n + 1:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[n] if dp[n] != float('inf') else -1
 
-input_str = input().split()
+    return dp[n] if dp[n] != n + 1 else -1
 
-n = int(input_str[0])
-total_sum = int(input_str[1])
+if __name__ == "__main__":
+    n, totalSum = map(int, input().split())
+    coins = list(map(int, input().split()))
 
-coins = list(map(int, input().split()))
-
-result = minimumcoins(total_sum, coins)
-print(result)
+    result = minimumCoins(totalSum, coins)
+    print(result)
