cc4871a168c228620f389b23b57e38f2c85d69ba
rasikasrimal <rasikacooldj620@gmail.com>
2023-10-26 10:01:40 +0530

    Exponentiation

--- CSES/C++/Mathematics/Exponentiation.cpp
+++ CSES/C++/Mathematics/Exponentiation.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-
-const long long MOD = 1000000007;
-
-long long fast_pow(long long a, long long b) {
-    if (b == 0)
-        return 1;
-    
-    long long res = fast_pow(a, b / 2) % MOD;
-    res = (res * res) % MOD;
-    
-    if (b % 2 == 1)
-        res = (res * a) % MOD;
-    
-    return res;
-}
+#include <vector>
+#include <cmath>
 
 int main() {
     int n;
     std::cin >> n;
+    std::vector<long long> numbers;
 
     for (int i = 0; i < n; ++i) {
-        long long a, b;
+        int a, b;
         std::cin >> a >> b;
-        long long result = fast_pow(a, b);
-        std::cout << result << std::endl;
+        long long result = std::pow(a, b);
+        result = result % 1000000007;
+        numbers.push_back(result);
+    }
+
+    for (long long number : numbers) {
+        std::cout << number << std::endl;
     }
 
     return 0;
