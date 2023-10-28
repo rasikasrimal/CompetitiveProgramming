2c315600325c3708a78f478dc0c562820b0406f5
rasikasrimal <rasikacooldj620@gmail.com>
2023-10-28 09:54:17 +0530

    countng divisors c++ less TC

---
+++ CSES/C++/Mathematics/Counting Divisors.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+// Function to calculate the number of divisors
+int countDivisors(int num) {
+    int divisors = 1; // Initialize with 1 to include num itself
+
+    for (int i = 2; i * i <= num; ++i) {
+        int exponent = 0;
+        while (num % i == 0) {
+            num /= i;
+            exponent++;
+        }
+        divisors *= (exponent + 1);
+    }
+
+    if (num > 1) {
+        // If there's a prime factor remaining
+        divisors *= 2;
+    }
+
+    return divisors;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers;
+
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        int divisors = countDivisors(num);
+        std::cout << divisors << std::endl;
+    }
+
+    return 0;
+}
