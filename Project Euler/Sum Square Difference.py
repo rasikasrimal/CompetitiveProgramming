c410c8a934ab7fb00543c26323daeaa4709d6a74
rasikasrimal <rasikacooldj620@gmail.com>
2023-11-05 21:48:40 +0530

    Sum Square Difference

---
+++ Project Euler/Sum Square Difference.py
@@ -0,0 +1,8 @@
+n = 100
+
+sum_of_squares = sum([i**2 for i in range(1, n+1)])
+square_of_sum = sum([i for i in range(1, n+1)])**2
+
+print(sum_of_squares)
+print(square_of_sum)
+print(square_of_sum - sum_of_squares)
