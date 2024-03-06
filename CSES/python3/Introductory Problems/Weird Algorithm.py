10e6dc00ead4c5e801e9d8f1f881054d7382cbda
rasikasrimal <rasikacooldj620@gmail.com>
2024-03-06 14:24:28 +0530

    Merge Folders

--- CSES/python3/Introductory Problems/Weird Algorithm.py
+++ CSES/python3/Introductory Problems/Weird Algorithm.py
@@ -6,17 +6,3 @@ while n > 1:
     else:
         n = int(3 * n + 1)
 print(n)
-
-# n = int(input())
-# sequence = []
-
-# while n > 1:
-#     sequence.append(n)
-#     if n % 2 == 1:
-#         n = 3 * n + 1
-#     else:
-#         n = n // 2
-
-# sequence.append(1)
-
-# print(*sequence)
