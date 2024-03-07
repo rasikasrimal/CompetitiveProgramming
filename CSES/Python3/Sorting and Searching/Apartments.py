abd014ca26b0e70b660b0b2f1da0d095cc732794
rasikasrimal <rasikacooldj620@gmail.com>
2024-03-07 06:38:12 +0530

    Apartments Solved

--- CSES/Python3/Sorting and Searching/Apartments.py
+++ CSES/Python3/Sorting and Searching/Apartments.py
@@ -3,10 +3,16 @@ n = int(user_input[0])
 m = int(user_input[1])
 k = int(user_input[2])
 
+count = 0
 
-user_size = input("").split(" ") #n elements
-apartment_size = input("").split(" ") #m elements
+user_size = list(map(int, input("").split(" ")))
+apartment_size = list(map(int, input("").split(" ")))
 
-print(n, m, k)
-print(user_size)
-print(apartment_size)
+for i in range(m):
+    for j in range(n):
+        if apartment_size[i] != -1 and user_size[j] - k <= apartment_size[i] <= user_size[j] + k:
+            count += 1
+            apartment_size[i] = -1
+            break
+
+print(count)
