f94696933b470d4976b97cf824071bde4b0137de
rasikasrimal <rasikacooldj620@gmail.com>
2024-08-12 23:03:13 +0530

    459. Repeated Substring Pattern

--- Leetcode/Programming SKills/Python3/459. Repeated Substring Pattern.py
+++ Leetcode/Programming SKills/Python3/459. Repeated Substring Pattern.py
@@ -1,5 +1,14 @@
-class Solution {
-    public boolean repeatedSubstringPattern(String s) {
-        
-    }
-}
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n = len(s)
+        for i in range(1, n // 2 + 1):  
+            if n % i == 0: 
+                subString = s[:i]  
+                repeated = subString * (n // i)  
+                if repeated == s: 
+                    return True
+        return False
